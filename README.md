19d10bb7dc9bf7701bc6007da06185b55f2c8460
Artico <artico.bandurini@gmail.com>
2019-06-20 10:38:46 +0300

    code cleanup, readme update, config setup

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## Leapdao
 
 JSON proxy for glassfrog API
+
+## Install
+
+```
+yarn
+```
+
+## Deploy
+
+> Use different stage (`-s bla`) to deploy for testing
+
+```
+API_TOKEN=<glassfrog token> sls deploy -s prod
+```
--- handler.js
+++ handler.js
@@ -2,21 +2,12 @@
 
 const fetch = require('node-fetch');
 
-module.exports.roles = (event, context, callback) => {
-  fetch('https://api.glassfrog.com/api/v3/roles', {
-    headers: {
-      'X-Auth-Token': process.env.API_TOKEN
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    const response = {
-      statusCode: 200,
+module.exports.roles = async event => {
+  const url = 'https://api.glassfrog.com/api/v3/roles';
+  const response = await fetch(url, {
       headers: {
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: data
-    };
-    callback(null, response);
+        'X-Auth-Token': process.env.API_TOKEN
+      }
   });
+  return await response.json();
 };
